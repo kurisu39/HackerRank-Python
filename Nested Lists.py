5c0ee9f275f262761195917e496332dfc4e672ba
kriz <83438483+kurisu39@users.noreply.github.com>
2022-12-19 12:59:04 -0500

    avences hoy

--- Nested Lists.py
+++ Nested Lists.py
@@ -1,4 +1,22 @@
 if __name__ == '__main__':
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())  
+    #for _ in range(int(input())):
+        #name = input()
+        #score = float(input())  
+    lst = []
+    n = int(input("Enter number of elements : "))
+    min=0
+    for i in range(0, n):
+        ele = [input(), float(input())]
+        
+    #second part
+        if ele[1]>min:
+            min=ele[1]   
+        
+    #third part
+        if ele [1]==min:
+            print (ele[0])
+        
+    #print (lst)
+    #print (min)
+    #print (sorted(result))
+    #print (lst[1])
