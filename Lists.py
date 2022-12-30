aebcdb029521d766ed1cf0ddbd45be64ce660eb5
kriz <83438483+kurisu39@users.noreply.github.com>
2022-12-30 13:17:02 -0500

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,7 +1,28 @@
 if __name__ == '__main__':
     N = int(input())
+    arr = list()
     
     for _ in range (N):
-        input ()
+        enter= (input ()).split(' ')
         
-    
+        if enter[0]=="insert":
+            arr.insert(int(enter[1]),int (enter[2]))
+
+        if enter[0]=="print":
+            print(arr)
+
+        if enter[0]=="remove":
+            arr.remove(int(enter[1]))
+            
+        if enter[0]=="append":
+            arr.append(int(enter[1]))
+            
+        if enter[0]=="sort":
+            arr.sort
+
+        if enter[0]=="pop":
+            arr.pop()
+            
+        if enter[0]=="reverse":
+            arr.reverse()
+            
