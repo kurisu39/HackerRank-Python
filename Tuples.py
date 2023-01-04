93047e4a3c529fd1a4e6ac474d70459b2831ef91
kriz <83438483+kurisu39@users.noreply.github.com>
2023-01-04 18:17:19 -0500

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     n = int(input())
-    integer_list = map(int, input().split())
+    integer_list = tuple(map(int, input().split()))
     
     for _ in range(n):
         break
-    print (integer_list)
+    print (hash(integer_list))
