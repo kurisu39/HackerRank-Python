e9f8902ec3d82292d226a317fb118abbd0bb58d9
kriz <83438483+kurisu39@users.noreply.github.com>
2023-01-13 17:00:26 -0500

    Update What's Your Name.py

--- Python/What's Your Name.py
+++ Python/What's Your Name.py
@@ -1,17 +1,9 @@
-#
-# Complete the 'print_full_name' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. STRING first
-#  2. STRING last
-#
-
 def print_full_name(first, last):
     Nah='Hello '+first+' '+last+'! You just delved into python.'
-    # Write your code here
-    return Nah
+
+    return print (Nah)
+
 if __name__ == '__main__':
-    first=input()
-    last=input()
-    print (print_full_name(first,last))
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
