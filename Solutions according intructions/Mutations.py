b41bc03c4d18532bf5a60573219bf79b13a9d233
kriz <83438483+kurisu39@users.noreply.github.com>
2023-01-30 15:07:24 -0500

    Update Mutations.py

--- Solutions according intructions/Mutations.py
+++ Solutions according intructions/Mutations.py
@@ -1,6 +1,11 @@
 def mutate_string(string, position, character):
-    return
 
+    l=list(string)
+    l[position]=character
+    string = ''.join(l)
+  
+    
+    return string
 if __name__ == '__main__':
     s = input()
     i, c = input().split()
